05e79b3c23256041c5bc7a209e812cf30177c6a2
Lahiru Gayashan <72918623+LahiruGayashan@users.noreply.github.com>
2026-01-16 22:19:29 +0530

    Add ProductPriceCard widget and integrate into HomePage
    Implemented the ProductPriceCard widget to display product pricing details and added it to the HomePage under the 'For Sale and Low Cost' section. This enhances the UI by providing a dedicated card for products with price and quantity information.

--- grocery_splash_app/lib/pages/home_page/home_page.dart
+++ grocery_splash_app/lib/pages/home_page/home_page.dart
@@ -1,7 +1,10 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:grocery_splash_app/pages/home_page/widgets/search_bar.dart';
 import 'package:grocery_splash_app/widgets/app_bar/app_bar.dart';
 import 'package:grocery_splash_app/widgets/reusable/product_card.dart';
+import 'package:grocery_splash_app/widgets/reusable/product_price_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -21,26 +24,26 @@ class HomePage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SearchBox(),
-              Text("Explore Categories",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight:FontWeight.w700,
-              ),
+              Text(
+                "Explore Categories",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   ProductCard(
                     title: "vegitable",
-                    description: "Vegetables are parts of plants that are consumed by humans...",
+                    description:
+                        "Vegetables are parts of plants that are consumed by humans...",
                     titleColor: const Color.fromARGB(255, 255, 255, 255),
                     descColor: Color.fromARGB(255, 255, 255, 255),
                     smallBoxColor: Color(0xff9dffcc),
                     mainBoxColor: Color.fromARGB(255, 98, 79, 167),
                   ),
                   ProductCard(
-                     title: "Fish & Meat",
-                    description: "Fish is the flesh of an animal used for food, and by that definition...",
+                    title: "Fish & Meat",
+                    description:
+                        "Fish is the flesh of an animal used for food, and by that definition...",
                     titleColor: const Color.fromARGB(255, 254, 254, 254),
                     descColor: Color.fromARGB(255, 255, 255, 255),
                     smallBoxColor: Color(0xff9dffcc),
@@ -48,45 +51,56 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(
-                height: 12,
-              ),
+              SizedBox(height: 12),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   ProductCard(
                     title: "Vegetables",
-                     titleColor: Colors.black,
-                      description: "Vegetables are parts of plants that are consumed by humans...",
-                       descColor: Colors.black,
-                        mainBoxColor: Color(0XFFFFE500),
-                         smallBoxColor: Color(0XFFFF9900),
-                         ),
-                         ProductCard(
+                    titleColor: Colors.black,
+                    description:
+                        "Vegetables are parts of plants that are consumed by humans...",
+                    descColor: Colors.black,
+                    mainBoxColor: Color(0XFFFFE500),
+                    smallBoxColor: Color(0XFFFF9900),
+                  ),
+                  ProductCard(
                     title: "Vegetables",
-                     titleColor: Colors.black,
-                      description: "Vegetables are parts of plants that are consumed by humans...",
-                       descColor: Colors.black,
-                        mainBoxColor: Color(0XFFFFE500),
-                         smallBoxColor: Color(0XFFFF9900),
-                         ),
+                    titleColor: Colors.black,
+                    description:
+                        "Vegetables are parts of plants that are consumed by humans...",
+                    descColor: Colors.black,
+                    mainBoxColor: Color(0XFFFFE500),
+                    smallBoxColor: Color(0XFFFF9900),
+                  ),
                 ],
               ),
-              SizedBox(
-                height: 12,
-              ),
-              Text("For Sale and Low Cost",style: TextStyle(
-                fontSize: 20,
-                fontWeight:FontWeight.w700,
-              ),
+              SizedBox(height: 12),
+              Text(
+                "For Sale and Low Cost",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
               ),
 
-              
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ProductPriceCard(
+                    productName: "Washing Liquid",
+                    quntityUnit: "ML",
+                    productPrice: "340.21",
+                    productQuntity: "12",
+                  ),
+                  ProductPriceCard(
+                    productName: "Washing Liquid",
+                    quntityUnit: "g",
+                    productPrice: "321",
+                    productQuntity: "3",
+                  ),
+                ],
+              ),
             ],
           ),
         ),
-
-        
       ),
     );
   }
--- grocery_splash_app/lib/widgets/reusable/product_price_card.dart
+++ grocery_splash_app/lib/widgets/reusable/product_price_card.dart
@@ -1,10 +1,86 @@
 import 'package:flutter/material.dart';
 
 class ProductPriceCard extends StatelessWidget {
-  const ProductPriceCard({super.key});
+  final String productName;
+  final String productQuntity;
+  final String quntityUnit;
+  final String productPrice;
+
+  const ProductPriceCard({super.key,
+   required this.productName,
+   required this.productQuntity, 
+   required this.quntityUnit, 
+   required this.productPrice});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 220,
+      width: 180,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Color(0xff9e00ff),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 80,
+              width: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Color(0XFF06FFA5),
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(
+              productName,
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      productQuntity.toString() + quntityUnit ,
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                     "$productPrice\$",
+                      style: TextStyle(
+                        color: Color(0XFFFFE500),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    border: Border.all(color: Colors.white, width: 3),
+                  ),
+                  child: Center(child: Icon(Icons.add, color: Colors.white)),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
