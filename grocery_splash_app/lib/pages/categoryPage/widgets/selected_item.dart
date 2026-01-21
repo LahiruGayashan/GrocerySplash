40c2f5a6e5acd35c4e53259e3a1ac9557245fdf1
Lahiru Gayashan <72918623+LahiruGayashan@users.noreply.github.com>
2026-01-21 11:44:13 +0530

    Implement UI for SelectedItem widget
    Added layout and styling to the SelectedItem widget, including a container with rounded borders, a title, and a row displaying an icon and descriptive text for vegetables.

--- grocery_splash_app/lib/pages/categoryPage/widgets/selected_item.dart
+++ grocery_splash_app/lib/pages/categoryPage/widgets/selected_item.dart
@@ -5,6 +5,53 @@ class SelectedItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+     height: 600,
+     width: double.infinity,
+     decoration: BoxDecoration(
+      color: Color(0xffE0DCD6).withOpacity(0.29),
+      borderRadius: BorderRadius.circular(20),
+      border: Border.all(
+        color: Color(0xffE0DCD6).withOpacity(0.5),
+        width: 10,
+      )
+     ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Vegetables",style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 30,
+        
+            ),),
+           Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Color(0xffFFE500)
+                ),
+                child: Center(child:
+                 Icon(Icons.numbers,
+                  color: Colors.black
+                  )
+                  ),
+              ),
+              Text("dataVegetables are parts of plants that are \nconsumed by humans...",
+              style: TextStyle(fontWeight: FontWeight.w700,fontSize: 14,
+              ),
+              ),
+            ],
+           )
+        
+          ],
+        ),
+      ),
+    );
   }
 }
